462c1ae37bc8164a2f65325efac2716f5b162dd8
Andre <andreas79001@gmail.com>
2023-10-03 20:35:12 +0200

    Starting impl of board & hand logic

--- Assets/Scripts/CardLogic/Board.cs
+++ Assets/Scripts/CardLogic/Board.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    private Deck DealerDeck;
+    private Player player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.DealerDeck = new Deck();
+        this.player = new Player();
+    }
+
+    void RegisterCard()
+    {
+
     }
 
     // Update is called once per frame
