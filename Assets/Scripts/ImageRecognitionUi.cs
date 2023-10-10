4f118b100d4282d131dba5255565556d2ad2a748
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-10-10 11:52:26 +0200

    Update ImageRecognitionUi.cs
    Taget Andreas kode, og sat i

--- Assets/Scripts/ImageRecognitionUi.cs
+++ Assets/Scripts/ImageRecognitionUi.cs
@@ -1,15 +1,19 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using CardEums; // Add this line to include the CardEums namespace
 
 public class ImageRecognitionUi : MonoBehaviour
 {
     public ARTrackedImageManager trackedImageManager;
     public TMP_Text  recognitionText;
     private int Counter;
+    //Reffrence to boardScript
+    public Board boardScript;
 
     private void OnEnable()
     {
@@ -27,9 +31,24 @@ public class ImageRecognitionUi : MonoBehaviour
 
         foreach (var trackedImage in eventArgs.added)
         {
+            
             Counter++;
             Debug.Log("Image added: " + trackedImage.referenceImage.name + "Counter = " + Counter);
             UpdateRecognitionText(trackedImage);
+            
+            //Adskil reffrence name og lav carded ud af det
+            var imageName = trackedImage.referenceImage.name;
+            var imageNameCpy = imageName.Split('_');
+
+
+            Suits suit = (Suits)Enum.Parse(typeof(Suits), imageNameCpy[1], ignoreCase: true);
+            CardType type = (CardType)Enum.Parse(typeof(CardType), imageNameCpy[0], ignoreCase: true);
+                    
+            // Register the new card in the codebase
+            Card c = new Card(type, suit);
+            boardScript.Start();
+            boardScript.player.RegisterCard(c);
+
         }
 
         foreach (var trackedImage in eventArgs.updated)
