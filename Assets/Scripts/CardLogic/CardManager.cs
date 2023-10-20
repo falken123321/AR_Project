453b1fd2ec432de677dfe3132244e9a6e197f7f0
Mark Hjuler Pedersen <73698747+BuilderMARK@users.noreply.github.com>
2023-10-20 15:17:39 +0200

    hh

--- Assets/Scripts/CardLogic/CardManager.cs
+++ Assets/Scripts/CardLogic/CardManager.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEditor;
 
 namespace CardLogic
 {
@@ -6,38 +10,70 @@ namespace CardLogic
 
     public class CardManager : MonoBehaviour
     {
-        
-        public GameObject cardPrefab; 
+
+        public GameObject cardPrefab;
         public Transform cardParent;
         public Sprite[] cardSprites;
         public Deck deck1;
+        public TextMeshProUGUI button;
+        public List<Card> drawnCards = new List<Card>();
+
+        private int cardsShown = 0;
 
         public void ShowFlop()
         {
             var cards = deck1.DrawRandomFlopCards();
-    
-            foreach (var card in cards)
+            Debug.Log(cardsShown);
+            if (cardsShown >= 3)
             {
-                GameObject cardObj = Instantiate(cardPrefab, cardParent.position, Quaternion.identity, cardParent);
-        
-                Sprite newSprite = cardSprites.FirstOrDefault(s => s.name == card.type.ToString() + "_" + card.suit.ToString());
-                cardObj.GetComponent<CardDisplay>().SetCardSprite(newSprite);
-                
-                cardObj.transform.position += new Vector3(4f * cards.IndexOf(card), 0, 0); 
+                button.text = "Show Final Card";
+                Debug.Log("funktionen Show Next bliver kaldt fra ShowFlop");
+                ShowNextCard();
+            }
+            else
+            {
+                foreach (var card in cards)
+                {
+                    cardsShown++;
+                    Debug.Log(cards.Count);
+                    GameObject cardObj = Instantiate(cardPrefab, cardParent.position, Quaternion.identity, cardParent);
+
+                    Sprite newSprite =
+                        cardSprites.FirstOrDefault(s => s.name == card.type.ToString() + "_" + card.suit.ToString());
+                    cardObj.GetComponent<CardDisplay>().SetCardSprite(newSprite);
+
+                    cardObj.transform.position += new Vector3(4f * cards.IndexOf(card), 0, 0);
+                    drawnCards.Add(card);
+
+                    if (cardsShown == 3)
+                    {
+                        button.text = "Show Next Card";
+                    }
+                }
+
             }
         }
         
+        
+        public List<Card> GetDrawnCards()
+        {
+            Debug.Log(drawnCards);
+            return drawnCards;
+        }
 
 
         public void ShowNextCard()
-        {
-            var card = deck1.DrawNextCard();
-            GameObject cardObj = Instantiate(cardPrefab, cardParent);
-            
-            Sprite newSprite = cardSprites.FirstOrDefault(s => s.name == card.type.ToString() + "_" + card.suit.ToString());
+            {
+                Debug.Log("funktionen Show Next");
+                var card = deck1.DrawNextCard();
+                GameObject cardObj = Instantiate(cardPrefab, cardParent);
 
-            cardObj.GetComponent<CardDisplay>().SetCardSprite(newSprite);
+                Sprite newSprite =
+                    cardSprites.FirstOrDefault(s => s.name == card.type.ToString() + "_" + card.suit.ToString());
+
+                cardObj.GetComponent<CardDisplay>().SetCardSprite(newSprite);
+                drawnCards.Add(card);
+            }
         }
-    }
 
-}
+    }
